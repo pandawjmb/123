49163a11a21527fb3faa6cc523ed00fc73e51fbc
xiongwanmin <1109786150@qq.com>
2019-05-24 15:18:22 +0800

    提交helloWord！你好ld！

--- src/main/java/cn/ld/com/HelloWord.java
+++ src/main/java/cn/ld/com/HelloWord.java
@@ -1,6 +1,6 @@
 package cn.ld.com;
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("helloword");
+        System.out.println("helloword你好ld");
     }
 }
